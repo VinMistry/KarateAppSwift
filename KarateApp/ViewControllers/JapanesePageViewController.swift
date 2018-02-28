2d64709c5957c1a5c000fa378e261f618a27f0c1
Vinesh Mistry <vm2amt@bolton.ac.uk>
2018-02-28 16:57:01 +0000

    Added Page View Controller for learning storyboard. This will allow for example numbers to be learning in text audio and video without making the screen to crowded

---
+++ KarateApp/ViewControllers/JapanesePageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  JapanesePageViewController.swift
+//  KarateApp
+//
+//  Created by Vinesh Mistry on 28/02/2018.
+//  Copyright © 2018 Vinesh Mistry. All rights reserved.
+//
+
+import UIKit
+
+class JapanesePageViewController: UIPageViewController , UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    lazy var subViewControllers:[UIViewController] = {
+        return [
+            UIStoryboard(name: "Learning", bundle: nil).instantiateViewController(withIdentifier: "JapaneseText") as! JapaneseTextViewController,
+            UIStoryboard(name: "Learning", bundle: nil).instantiateViewController(withIdentifier: "JapaneseAudio") as! JapaneseAudioViewController,
+            UIStoryboard(name: "Learning", bundle: nil).instantiateViewController(withIdentifier: "JapaneseVideo") as! JapaneseVideoViewController
+        ]
+    }()
+    
+    var pageContr = UIPageControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.dataSource = self
+        
+        // Do any additional setup after loading the view.
+        setViewControllers([subViewControllers[0]], direction: .forward, animated: true, completion: nil)
+        configPageCController()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configPageCController() {
+        pageContr = UIPageControl(frame: CGRect(x: 0, y: UIScreen.main.bounds.maxY-50, width: UIScreen.main.bounds.width, height: 50))
+        pageContr.numberOfPages = subViewControllers.count
+        pageContr.currentPage = 0
+        pageContr.tintColor = UIColor.blue
+        pageContr.pageIndicatorTintColor = UIColor.black
+        pageContr.currentPageIndicatorTintColor = UIColor.white
+        self.view.addSubview(pageContr)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageContr.currentPage = subViewControllers.index(of: pageContentViewController)!
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return subViewControllers.count
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let currentIndex: Int = subViewControllers.index(of: viewController) ?? 0
+        if(currentIndex <= 0 ) {
+            return nil
+        }
+        return subViewControllers[currentIndex-1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let currentIndex: Int = subViewControllers.index(of: viewController) ?? 0
+        if(currentIndex >= subViewControllers.count-1 ) {
+            return nil
+        }
+        return subViewControllers[currentIndex+1]
+    }
+
+}
---
+++ KarateApp/ViewControllers/JapaneseVideoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JapaneseVideoViewController.swift
+//  KarateApp
+//
+//  Created by Vinesh Mistry on 28/02/2018.
+//  Copyright © 2018 Vinesh Mistry. All rights reserved.
+//
+
+import UIKit
+
+class JapaneseVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
